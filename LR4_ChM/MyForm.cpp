0ad3ee51d7ed8e1dda81da2f5a1530b0b5bb31f2
PlohovEvg <plohov99@yandex.ru>
2020-03-28 19:55:10 +0300

    v. 1.0

---
+++ LR4_ChM/MyForm.cpp
@@ -0,0 +1,14 @@
+#include "MyForm.h"
+
+using namespace LR4_ChM;
+
+[STAThreadAttribute]
+int main(cli::array<System::String ^> ^args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	Application::Run(gcnew MyForm());
+	return 0;
+}
+
